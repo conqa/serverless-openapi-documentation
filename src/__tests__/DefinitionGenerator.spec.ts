01c12187b05832c6c372350591bea6e1d671dc8f
wrumsby <walter@rumsby.net>
2019-05-07 11:27:56 +1200

    lint

--- src/__tests__/DefinitionGenerator.spec.ts
+++ src/__tests__/DefinitionGenerator.spec.ts
@@ -1,5 +1,5 @@
 import * as path from 'path';
-import * as Serverless from 'serverless';
+import Serverless from 'serverless';
 import { DefinitionGenerator } from '../DefinitionGenerator';
 
 class ServerlessInterface extends Serverless {

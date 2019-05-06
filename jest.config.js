a19f2b647f76e19e372b0cac0721dc184d2afadd
wrumsby <walter@rumsby.net>
2019-05-07 11:41:06 +1200

    tests

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,6 @@
 module.exports = {
   "verbose": true,
-  "transform": { ".(ts|tsx)": "<rootDir>/node_modules/ts-jest/preprocessor.js" },
-  "testRegex": "(/__tests__/.*|\\.(test|spec))\\.(ts|tsx|js)$",
-  "moduleFileExtensions": [ "ts", "tsx", "js" ],
+  "preset": "ts-jest",
   "coverageDirectory": ".coverage",
   "testPathIgnorePatterns": [
     "/node_modules/",
